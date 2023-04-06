384207e140bcbac97956f373760023e8348d7159
RyukungG <ryu.nosuke@hotmail.com>
2023-04-06 16:27:32 +0700

    finish lend

--- app.js
+++ app.js
@@ -53,6 +53,30 @@ const getBooks = async(res) => {
     }
 }
 
+const getLends = async(res) => {
+    /* Retrieves all rows from the `Book` table in the database */
+    let con;
+    try {
+        // Connect to the database using the connection string
+        con = await sql.connect(string_connection)
+        // Create a new SQL request object
+        let request = new sql.Request(con);
+        const result = await request.query('select * from Lending');
+        console.log(string_connection);
+        return result;
+    }
+    catch (err) {
+        console.log(string_connection);
+        res.status(500).send('Error connecting to the database');
+
+    } finally {
+        // Close the database connection
+        if (con) {
+            con.close();
+        }
+    }
+}
+
 // Set up the Express.js application
 var express = require("express");
 var cors = require("cors");
@@ -94,6 +118,66 @@ app.get("/books", async (req, res) => {
 app.get('/', function(req, res) {
     res.render('pages/home')
 })
+
+app.get("/lend", async function (req, res) {
+  let results = await getLends(res);
+  res.render("lending", {
+    resultsLending: results.recordset,
+  });
+});
+
+app.post("/lend/create", async function(req, res){
+     // Connect to the database using the connection string
+     con = await sql.connect(string_connection)
+     // Create a new SQL request object
+     let request = new sql.Request(con);
+
+    let getMaxLendIdQuery = "SELECT MAX(Lending_id) AS max_id FROM Lending";
+    let maxLendIdResult = await request.query(getMaxLendIdQuery);
+    let newLendId = maxLendIdResult.recordset[0].max_id + 1;
+    
+    const member = req.body.member_id;
+    const book = req.body.book_id;
+    const borrow = req.body.borrow_date;
+
+    request.input('Lending_id', sql.Int, newLendId);
+    request.input('Member_id', sql.Int, member);
+    request.input('Book_id', sql.Int, book);
+    request.input('Borrow_date', sql.Date, borrow);
+
+    request.execute('createLending', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Something went wrong!');
+        } else {
+            res.send(result.recordset);
+        }
+    });
+});
+
+app.post("/lend/return", async function(req, res){
+    // Connect to the database using the connection string
+    con = await sql.connect(string_connection)
+    // Create a new SQL request object
+    let request = new sql.Request(con);
+   
+   const member = req.body.member_id;
+   const book = req.body.book_id;
+   const return_date = req.body.return_date;
+
+   request.input('Member_id', sql.Int, member);
+   request.input('Book_id', sql.Int, book);
+   request.input('Return_date', sql.Date, return_date);
+
+   request.execute('returnLending', (err, result) => {
+       if (err) {
+           console.log(err);
+           res.status(500).send('Something went wrong!');
+       } else {
+           res.send(result.recordset);
+       }
+   });
+});
 
 // Start the server on port 3000
 const port = 3000;
